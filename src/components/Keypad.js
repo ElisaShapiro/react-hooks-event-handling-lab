78238ad5def7f27f3bed2ad551f78abcf29daf64
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-21 17:08:51 -0400

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+function Keypad(){
+    function handleChange(event) {
+        console.log(`Entering password...`)
+    }
+    return (
+        <input onChange={handleChange} type="password">
+        </input>
+    )
+}
+
+export default Keypad;
